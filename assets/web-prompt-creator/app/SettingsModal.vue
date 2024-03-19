832e4245f077649014ca10485a985a496b421618
genosajd <genosajd@gmail.com>
2024-03-19 06:57:29 +0000

    fix

--- assets/web-prompt-creator/app/SettingsModal.vue
+++ assets/web-prompt-creator/app/SettingsModal.vue
@@ -16,7 +16,7 @@
     <div v-if="showModal && isMaximized" class="settings-modal" ref="target"  :style="'position: fixed; transform: translate(-50%, -50%); width:'+width+'px;'">
       <div class="settings-modal__header" ref="targetHeader">
         <h2>{{ modalTitle }}</h2>
-        <i class="bi bi-arrow-bar-down pointer" @click="minimize()"></i>
+        <i v-if="props.elementData !== null" class="bi bi-arrow-bar-down pointer" @click="minimize()"></i>
       </div>
       <div class="settings-modal__body">
         <slot></slot>
@@ -63,7 +63,7 @@ const props = defineProps({
     },
     elementData: {
         type: Object,
-        default: {}
+        default: null
     }
 });
 let showModal = ref(props.show);
